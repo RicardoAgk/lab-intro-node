1b54eaa42658c300762519326f671d3b02dae1ee
RicardoAgk <ricardoandre.dasein@gmail.com>
2020-11-02 16:23:56 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a, b){return a - b});
+    this.length = this.items.length
+  }
+    
+  get(pos) {
+    if(this.items.includes(pos)){
+      return this.items[pos];
+    }else{
+      throw new Error('OutOfBounds');
+    }
+     
+  }
 
-  get(pos) {}
+  max() {
+    if(this.items.length > 0){
+       return Math.max.apply(null, this.items)
+    }else{
+      throw new Error('EmptySortedList');
+    }
+    
+  }
 
-  max() {}
+  min() {
+    if(this.items.length > 0){
+      return Math.min.apply(null, this.items)
+   }else{
+     throw new Error('EmptySortedList');
+   }
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  sum() {
+    let sum = 0;
+    for(let i = 0; i<this.items.length; i++){
+      sum += this.items[i]
+    }
+    return sum
+}
+  avg() {
+    let avg = 0;
+    if(this.items.length > 0){
+      for(let i = 0; i<this.items.length; i++){
+      avg += this.items[i]
+    }
+      return avg / this.items.length
+    }else{
+      throw new Error('EmptySortedList');
+    }
+    }
+    
 }
 
 module.exports = SortedList;
